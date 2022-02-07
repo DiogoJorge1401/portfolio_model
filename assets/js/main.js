c97771d94e8c677fba6fa62b1093b20b5eb134ba
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-02-07 15:02:59 -0300

    finished project

---
+++ assets/js/main.js
@@ -0,0 +1,135 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById('header')
+  if (this.scrollY >= 50) header.classList.add('scroll-header')
+  else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+  modalBtns = document.querySelectorAll('.services__button'),
+  modalClose = document.querySelectorAll('.services__modal-close')
+
+const modal = (modalClickIndex) => {
+  modalViews[modalClickIndex].classList.add('active-modal')
+}
+const close = (modalClickIndex) => {
+  modalViews[modalClickIndex].classList.remove('active-modal')
+}
+
+modalBtns.forEach((el, idx) => {
+  el.addEventListener('click', () => modal(idx))
+})
+
+modalClose.forEach((el, idx) => {
+  el.addEventListener('click', () => close(idx))
+})
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+const mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+    target: '.work__card',
+  },
+  animation: {
+    duration: 400,
+  },
+})
+/* Link active work */
+const linkWorks = document.querySelectorAll('.work__item')
+
+function activeWork(linkWorkIndex) {
+  linkWorks.forEach((el) => {
+    el.classList.remove('active-work')
+  })
+  this.classList.add('active-work')
+}
+
+linkWorks.forEach((el, idx) => el.addEventListener('click', activeWork))
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+const swiperTestimonial = new Swiper('.testimonial__container', {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+})
+swiperTestimonial.setGrabCursor()
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link')
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+/*=============== LIGHT DARK THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    lightTheme
+  )
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](
+    iconTheme
+  )
+}
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(lightTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true,
+})
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, {
+  delay: 700,
+})
+sr.reveal(`.home__social, .home__scroll`, {
+  delay: 900,
+  origin:'bottom'
+})
